eeb45261c2645a3b4c7096c523fb141e341355cf
Marco Bucci <56220142+MACULINX@users.noreply.github.com>
2023-10-25 08:15:15 +0200

    update

--- Bucci.Marco.4I.RubricaWPF/Bucci.Marco.4I.RubricaWPF/MainWindow.xaml.cs
+++ Bucci.Marco.4I.RubricaWPF/Bucci.Marco.4I.RubricaWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Bucci.Marco._4I.RubricaWPF
 {
@@ -27,27 +29,43 @@ namespace Bucci.Marco._4I.RubricaWPF
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Contatto c = new Contatto();
+            int idx = 0;
+            Contatto[] Contatti = new Contatto[100];
 
-            //Qui scateniamo il setter
+            
+
             try
             {
-                c.Numero = 101;
+                StreamReader buffer = new StreamReader("Dati.csv");
+                while (!buffer.EndOfStream)
+                {
+                    string row = buffer.ReadLine();
+                    Contatto contatto = new Contatto(row);
 
+                    Contatti[idx++] = contatto;
+                }
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
 
+            for (int i = 0; i < Contatti.Length; i++)
+                if (Contatti[i] == null)
+                    Contatti[i] = new Contatto();
+
+            
+            /*
+            Contatto c = new Contatto();
+
             c.Nome = "Marco";
             c.Cognome = "Bucci";
+            c.Email = "marco.bucci@studenti.ittsrimini.edu.it";
 
-
-
-
-            Contatto[] Contatti = new Contatto[100];
             Contatti[0] = c;
+            */
+
+            dgArray.ItemsSource = Contatti;
 
         }
     }
